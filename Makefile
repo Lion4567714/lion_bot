126ceb843f35a2b9909d6e8dd740113064effc6c
Lion4567714 <ablion7714@icloud.com>
2024-01-29 22:51:11 -0500

    working commands

--- Makefile
+++ Makefile
@@ -1,5 +1,4 @@
+.PHONY: bot
+
 bot:
 	@ python3 ./bot/lion_bot.py
-
-data:
-	@ python3 ./data/load_data.py
--- bot/lion_bot.py
+++ bot/lion_bot.py
@@ -1,41 +1,62 @@
+############### IMPORTS ###############
 import discord
-from discord.ext import commands
+from discord import app_commands
 import logging
+
 import pymongo
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-import message_processor as mp
-import sys
 
+import sys
+import urllib
+import message_processor as mp
+#######################################
+
+
+# Discord Client
 intents = discord.Intents.all()
 intents.message_content = True
-bot = commands.Bot(command_prefix='/', intents=intents)
+discord_client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(discord_client)
 
 # Logging
 handler = logging.FileHandler(filename='./logs/lion_bot.log', encoding='utf-8', mode='w')
 
-# MongoDB
+# Guilds
+# TODO: Move guilds to a file and read them in
+guilds = [discord.Object(id=976251686270144522), discord.Object(id=307725570429550592)]
+
+############### MONGO #################
 file = open('./config/mongo', 'r')
 mongo = file.read().splitlines()
 file.close()
-uri = f"mongodb+srv://{mongo[0]}:{mongo[1]}@discordcluster.ujcowjs.mongodb.net/?retryWrites=true&w=majority"
+
+mongo_user = urllib.parse.quote(mongo[0])
+mongo_pass = urllib.parse.quote(mongo[1])
+
+uri = f"mongodb+srv://{mongo_user}:{mongo_pass}@discordcluster.ujcowjs.mongodb.net/?retryWrites=true&w=majority"
 mongo_client = MongoClient(uri, server_api=ServerApi('1'))
-db = mongo_client.my_database
 
 try:
     mongo_client.admin.command('ping')
+    # logging.info()
     print("Ping successful. Connected to MongoDB!")
 except Exception as e:
+    print("Ping unsuccessful. Connected to MongoDB failed!")
     print(e)
 
+db = mongo_client.my_database
 posts = db['posts']
-# End MongoDB
+#######################################
 
-@bot.event
+
+@discord_client.event
 async def on_ready():
-    print(f'Logged into discord as {bot.user}!')
+    for guild in guilds:
+        await tree.sync(guild=guild)
+    print(f'Logged into discord as {discord_client.user}!')
 
-@bot.event
+@discord_client.event
 async def on_message(message):
     post = mp.message_to_dict(message)
     print(post)
@@ -45,23 +66,39 @@ async def on_message(message):
         logging.error("Something went wrong with sending a message to MongoDB!")
         sys.exit(1)
 
-    if message.author == bot.user:
+    if message.author == discord_client.user:
         return
 
     response = mp.get_response(message.content)
     if len(response) != 0:
         await message.channel.send(response)
+        
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong') 
+############# COMMANDS ################
+@tree.command(name='ping', guilds=guilds)
+async def ping(interaction):
+    await interaction.response.send_message('pong')
 
-@bot.command()
-async def test(ctx):
-    await ctx.send("got the test") 
+@tree.command(name='test', guilds=guilds)
+async def test(interaction):
+    await interaction.response.send_message('Go fuck yourself')
 
+# @tree.command(name='disconnect', guilds=guilds)
+# async def disconnect(interaction: discord.Interaction, member: discord.Member):
+#     voice_state = member.voice
+
+#     if voice_state is None:
+#         # Exiting if the user is not in a voice channel
+#         return await interaction.response.send_message('Hey dumbass, you have to be in voice to get disconnected.\nTry thinking a little bit before using random commands, okay?')
+#     else:
+#         await member.move_to(None)
+
+#######################################
+
+
+# Final initialization
 file = open('./config/token', 'r')
 token = file.read()
 file.close()
 
-bot.run(token, log_handler=handler, log_level=logging.INFO)
+discord_client.run(token, log_handler=handler, log_level=logging.INFO)
